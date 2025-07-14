10d68373ed28c798819acd0e0a4174af7dcaf08d
GrayHut <ondiekiowen99@gmail.com>
2025-07-15 01:47:34 +0300

    updating assignment(Python basics assignment)

---
+++ lux_practice/elif.py
@@ -0,0 +1,104 @@
+# def grade_sys():
+#     grade=int(input("Enter your grade: "))
+
+#     if grade <= 40:
+#         #print("E")
+#         return "E"
+
+#     elif grade <=50:
+#         #print("D")
+#         return "D"
+
+#     elif grade <=60:
+#         #print("C")
+#         return "C"
+
+#     elif grade <= 70:
+#         #print("B")
+#         return "B"
+
+#     elif grade <= 100:
+#         #print("A")
+#         return "A"
+
+#     else:
+#         print("Invalid grade input")
+
+# print(grade_sys())
+
+
+
+revenue_input=int(input("Enter your revenue: "))
+cost_input=int(input("Enter your cost: "))
+
+
+def profit_sys(revenue_input, cost_input):
+    
+    # revenue_input=int(revenue_input)
+    # cost_input=int(cost_input)
+    profit=revenue_input-cost_input
+    return profit
+
+print(profit_sys(revenue_input, cost_input))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lux_practice/weather.py
@@ -0,0 +1,8 @@
+income = float(input("Enter your income before taxes: "))
+#net=int(income*0.56)
+
+def after_tax():
+     net=int(income*0.56)
+     print(net)
+
+after_tax()
